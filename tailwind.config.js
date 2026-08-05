6bebc8baf095465c3192a3e9710a238a3cdadddb
ReihanMutaqin <reyhanmutakin1@gmail.com>
2026-08-05 14:28:05 +0700

    style: set global typography to ITC Lubalin Graph font stack with web slab-serif fallbacks

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"ITC Lubalin Graph"', '"Lubalin Graph"', '"Roboto Slab"', '"Arvo"', '"Zilla Slab"', 'Rockwell', 'serif'],
+        serif: ['"ITC Lubalin Graph"', '"Lubalin Graph"', '"Roboto Slab"', '"Arvo"', '"Zilla Slab"', 'Rockwell', 'serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
